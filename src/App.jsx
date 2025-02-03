71b2cdc4099df5e63945d57bf15924587cb4d465
thealmighty3096 <phanikumarc24@iimklive.com>
2025-02-03 15:45:01 +0530

    added analytics through vercel

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import AstrologicalServices from './components/AstrologicalServices'
 import ConsultationForm from './components/ConsultationForm'
 import Testimonials from './components/Testimonials'
 import FAQ from './components/FAQ'
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="bg-[#1a1a1a] text-white">
       <Navbar />
       <div>
+      <Analytics />
         <Hero />
         <ServiceSection />
         <AstrologicalServices />
